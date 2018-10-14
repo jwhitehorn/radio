a74d6f985b09d4fd6dc90bcfad7c51bcae6b380a
Jason Whitehorn <jason.whitehorn@gmail.com>
2018-10-14 16:43:25 +0000

    absolute paths to make crontab happy

--- main.rb
+++ main.rb
@@ -3,13 +3,14 @@
 require 'timeout'
 
 freq = 1420405751.786
-duration = 10 #minutes 
+duration = 720 #minutes 
+path = "/home/jason/radio/captures"
 
 
 now = Time.now
 
 filename = now.strftime("%Y%m%dT%H%M%SZ") + ".raw"
 
-cmd = "rtl_fm -f #{freq} ./captures/#{filename}"
+cmd = "/usr/local/bin/rtl_fm -f #{freq} #{path}/#{filename}"
 system "timeout #{duration}m #{cmd}"
-system "scp ./captures/#{filename} jason@saturn.lan.whitehorn.us:#{filename} && rm ./captures/#{filename}"
+system "scp #{path}/#{filename} jason@saturn.lan.whitehorn.us:#{filename} && rm #{path}/#{filename}"
